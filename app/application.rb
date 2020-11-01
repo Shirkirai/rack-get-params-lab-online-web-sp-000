b2ef39eb9548aaa31e01b7b16b82ca260bfb6c8c
Jung Lee <lee.jung.s95@gmail.com>
2020-11-01 20:55:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,11 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+
+    elsif req.path.match(/cart/)
+      @@cart.each do |cart_item|
+        resp.write "#{cart_item}\n"
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
