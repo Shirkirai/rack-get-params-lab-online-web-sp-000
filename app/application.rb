36d604e3253428a2c4bd3c87963d20c7865f1243
Jung Lee <lee.jung.s95@gmail.com>
2020-11-01 20:56:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,6 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-
     else
       resp.write "Path Not Found"
     end
